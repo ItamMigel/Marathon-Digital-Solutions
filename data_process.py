914150c0925cb7f2118fb7129cd7f0e4ebbe031b
OptonGroup-server <opton.group.server@outlook.com>
2025-03-29 02:18:20 +0300

    Оптимизация обработки данных в data_process.py: улучшено условие фильтрации столбцов, добавлены сообщения о времени выполнения и проверка на совпадение длины столбцов. Удален файл data.csv.

--- data_process.py
+++ data_process.py
@@ -12,16 +12,21 @@ def process_data(file_path: str = 'data/Телеметрия.xlsx', output_path:
     df = pd.read_excel(file_path)
     print(f'Data loaded')
     df_columns = df.columns
-    df_area_1 = df[[col for col in df_columns if not col.endswith('2')]]
-    df_area_2 = df[[col for col in df_columns if not col.endswith('1')]]
+    df_area_1 = df[[col for col in df_columns if '2' not in col]]
+    df_area_2 = df[[col for col in df_columns if '1' not in col]]
 
-    df_area_1.columns = [col.replace('1', '') for col in df_area_1.columns]
-    df_area_2.columns = [col.replace('2', '') for col in df_area_2.columns]
+    df_area_1.columns = [col.replace(' 1', '') for col in df_area_1.columns]
+    df_area_2.columns = [col.replace(' 2', '') for col in df_area_2.columns]
 
     df_all = pd.concat([df_area_1, df_area_2], axis=0)
+    print(f'Длина совпадает: {len(df_all.columns) == len(set(df_area_1.columns + df_area_2.columns))}')
     print(f'Data concatenated')
     df_all.to_parquet(output_path, index=False)
     print(f'Data saved to {output_path}')
 
 if __name__ == '__main__':
+    import time
+    start_time = time.time()
     process_data()
+    end_time = time.time()
+    print(f'Time taken: {end_time - start_time} seconds')
